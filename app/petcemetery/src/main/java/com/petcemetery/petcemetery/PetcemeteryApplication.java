e4cb7cea3d1202ea876dcfa21b86ad2b42f266a6
lclcada <62142509+lclcada@users.noreply.github.com>
2023-05-09 19:54:27 -0300

    Add files via upload

---
+++ app/petcemetery/src/main/java/com/petcemetery/petcemetery/PetcemeteryApplication.java
@@ -0,0 +1,13 @@
+package com.petcemetery.petcemetery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcemeteryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetcemeteryApplication.class, args);
+	}
+
+}
