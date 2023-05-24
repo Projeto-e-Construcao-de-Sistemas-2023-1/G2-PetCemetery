6776a680bac5f10e6c5245be657326c453a10467
Guilherme <ggxaraujo@gmail.com>
2023-05-23 23:54:41 -0300

    Visualizar/comprar/selecionarornamento

--- petcemetery/src/main/java/com/petcemetery/petcemetery/controller/JazigoController.java
+++ petcemetery/src/main/java/com/petcemetery/petcemetery/controller/JazigoController.java
@@ -1,11 +1,15 @@
 package com.petcemetery.petcemetery.controller;
 
+import com.petcemetery.petcemetery.model.Carrinho;
 import com.petcemetery.petcemetery.model.Jazigo;
 import com.petcemetery.petcemetery.repositorio.JazigoRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+
 
 import java.util.List;
 import java.util.Optional;
@@ -14,7 +18,7 @@ import java.util.Optional;
 public class JazigoController {
 
     private final JazigoRepository jazigoRepository;
-
+    
     public JazigoController(JazigoRepository jazigoRepository) {
         this.jazigoRepository = jazigoRepository;
     }
@@ -33,6 +37,35 @@ public String comprarJazigo(@PathVariable("id") Long id, Model model) {
         Jazigo jazigo = optionalJazigo.get();
         model.addAttribute("jazigo", jazigo);
         return "comprarJazigo";
+    } else {
+        return "redirect:/jazigo";
+    }
+}
+@Autowired
+private Carrinho carrinho; // Supondo que você tenha uma instância de Carrinho injetada no controlador
+
+@GetMapping("/comprar/{id}/plano")
+public String selecionarPlano(@PathVariable("id") Long id, Model model) {
+    Optional<Jazigo> optionalJazigo = jazigoRepository.findById(id);
+    if (optionalJazigo.isPresent()) {
+        Jazigo jazigo = optionalJazigo.get();
+        carrinho.adicionarItem(jazigo, jazigo.getPlano()); // Adiciona o Jazigo e o Plano ao Carrinho
+        model.addAttribute("jazigo", jazigo);
+        model.addAttribute("planos", Jazigo.PlanoEnum.values());
+        model.addAttribute("totalCarrinho", carrinho.getTotalCarrinho()); // Adiciona o valor total do carrinho ao modelo
+        return "confirmarPagamento";
+    } else {
+        return "redirect:/jazigo";
+    }
+}
+@GetMapping("/comprar/{id}/plano/finalizar")
+public String finalizarPagamento(@PathVariable("id") Long id, Model model) {
+    Optional<Jazigo> optionalJazigo = jazigoRepository.findById(id);
+    if (optionalJazigo.isPresent()) {
+        Jazigo jazigo = optionalJazigo.get();
+        model.addAttribute("jazigo", jazigo);
+        model.addAttribute("carrinho", carrinho);
+        return "finalizarPagamento";
     } else {
         return "redirect:/jazigo";
     }
