5ec660439ed5cdaa5d72f279938ad1b238f481b3
lclcada <vittalinni@gmail.com>
2023-06-01 14:57:10 -0300

    mudanca no admin

--- petcemetery/src/main/java/com/petcemetery/petcemetery/model/Admin.java
+++ petcemetery/src/main/java/com/petcemetery/petcemetery/model/Admin.java
@@ -2,9 +2,11 @@ package com.petcemetery.petcemetery.model;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
+import lombok.NoArgsConstructor;
 
 @Entity(name = "Administrador")
 @Table(name = "Administrador")
+@NoArgsConstructor
 public class Admin extends Usuario{
 
     public Admin(String email, String telefone, String nome, String cpf, boolean admin, String senha) {
