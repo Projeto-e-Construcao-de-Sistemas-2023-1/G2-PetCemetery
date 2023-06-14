ede6262249f9dc33721ae308d9bebaca6bd026b9
GabrielRamiroBR <gabrielramirom@gmail.com>
2023-06-14 20:35:13 -0300

    CDU visualizar despesas rodando

---
+++ petcemetery/src/main/java/com/petcemetery/petcemetery/DTO/VisualizarDespesasDTO.java
@@ -0,0 +1,20 @@
+package com.petcemetery.petcemetery.DTO;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.petcemetery.petcemetery.model.Servico;
+
+import lombok.Data;
+
+//@JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+public class VisualizarDespesasDTO {
+    String endJazigo;
+    String tipoServico;
+    double valor;
+
+    public VisualizarDespesasDTO(Servico servico){
+        this.endJazigo = servico.getJazigo().getEndereco();
+        this.tipoServico = servico.getTipoServico().toString();
+        this.valor = servico.getValor();
+    }
+}
