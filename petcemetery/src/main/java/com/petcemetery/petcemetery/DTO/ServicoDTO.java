484dfc26d3a4c3e4175f8d1f5f8f07bab30a04da
Breno Figueiredo Borges <brenofigueiredoborges@gmail.com>
2023-06-23 21:18:52 -0300

    Vizualizar enterros, exumações e jazigos. Admin

--- petcemetery/src/main/java/com/petcemetery/petcemetery/DTO/ServicoDTO.java
+++ petcemetery/src/main/java/com/petcemetery/petcemetery/DTO/ServicoDTO.java
@@ -8,17 +8,21 @@ import lombok.Data;
 @Data
 public class ServicoDTO {
     
+    String cpfCliente;
     long idJazigo;
     double valor;
     ServicoEnum tipoServico;
     String enderecoJazigo;
+    long idPet;
     PlanoEnum plano;
 
-    public ServicoDTO(double valor, ServicoEnum tipoServico, String enderecoJazigo, PlanoEnum plano, long idJazigo) {
+    public ServicoDTO(double valor, ServicoEnum tipoServico, String enderecoJazigo, PlanoEnum plano, long idJazigo, long idPet, String cpfCliente) {
         this.idJazigo = idJazigo;
         this.valor = valor;
         this.tipoServico = tipoServico;
         this.enderecoJazigo = enderecoJazigo;
         this.plano = plano;
+        this.idPet = idPet;
+        this.cpfCliente = cpfCliente;
     }
 }
--- petcemetery/src/main/java/com/petcemetery/petcemetery/controller/CarrinhoController.java
+++ petcemetery/src/main/java/com/petcemetery/petcemetery/controller/CarrinhoController.java
@@ -164,7 +164,7 @@ public class CarrinhoController {
                     break;
             }
             
-            ServicoDTO servicoDTO = new ServicoDTO(valor, item.getServico(), item.getJazigo().getEndereco(), item.getPlano(), item.getJazigo().getIdJazigo());
+            ServicoDTO servicoDTO = new ServicoDTO(valor, item.getServico(), item.getJazigo().getEndereco(), item.getPlano(), item.getJazigo().getIdJazigo(), item.getPet().getId(), item.getCpfCliente());
 
             listaServicosDTO.add(servicoDTO);
         }
